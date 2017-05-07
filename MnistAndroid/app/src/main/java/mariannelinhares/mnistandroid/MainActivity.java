fd95f87871929fd0028e7bcbca47d254212604bc
Piasy <xz4215@gmail.com>
2017-05-07 12:25:49 +0800

    clean up Android project

--- MnistAndroid/app/src/main/java/mariannelinhares/mnistandroid/MainActivity.java
+++ MnistAndroid/app/src/main/java/mariannelinhares/mnistandroid/MainActivity.java
@@ -110,18 +110,6 @@ public class MainActivity extends Activity implements View.OnClickListener, View
         });
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
-
     @Override
     public void onClick(View view){
 
